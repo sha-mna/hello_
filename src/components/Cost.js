094735e8ba85d09c44cf6ffa8f9484ba6afd1a51
Shamna K <shamnashami500@gmail.com>
2024-06-24 00:32:27 +0530

    files added

---
+++ src/components/Cost.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Cost() {
+  return (
+    <div>
+        <h1>Hello World </h1>
+        <p>It is new World, welcome</p>
+      
+    </div>
+  )
+}
+
+export default Cost
